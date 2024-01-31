d2db7d8fb67e6f338baade989631399e9557390e
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-01-31 02:11:12 -0300

    Added eslint and prettier config

--- src/components/ReactComponent.tsx
+++ src/components/ReactComponent.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from 'react';
 
-export const ReactComponent = () => {
-  return (
-    <div className='bg-red-400'>ReactComponent</div>
-  )
-}
+export const ReactComponent: React.FunctionComponent = () => {
+  return <div className='bg-red-400'>ReactComponent</div>;
+};
