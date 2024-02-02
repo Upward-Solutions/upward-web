b9bd03275330e852ffdbade8812c5e2da53f18b4
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-02-01 21:05:29 -0300

    Modified base styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,10 +3,14 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        'dark-background': '#0d0d0d',
+      },
       fontFamily: {
         base: ['Space Grotesk Variable', 'sans-serif'],
       },
       fontSize: {
+        base: ['1rem', '1.5rem'],
         'page-title': ['3rem', '61px'],
       },
     },
