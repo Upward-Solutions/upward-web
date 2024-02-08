8beb06e8bf65d911ae616259f0b9229feb04b58b
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-02-08 19:34:29 -0300

    Added new Section component

---
+++ src/components/Section.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Section: React.FunctionComponent<Props> = ({ children }) => {
+  return (
+    <section className='even:bg-background-100 flex flex-col gap-8 p-16'>
+      {children}
+    </section>
+  );
+};
+
+export default Section;
