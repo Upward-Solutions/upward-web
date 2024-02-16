fb53f1fa222b8de335bbdaecc8bfc4eb94be35a9
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-02-15 21:36:45 -0300

    Added Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FunctionComponent = () => {
+  return (
+    <footer className='p-4 flex justify-center text-paragraph'>
+      Upward Solutions - 2024
+    </footer>
+  );
+};
+
+export default Footer;
